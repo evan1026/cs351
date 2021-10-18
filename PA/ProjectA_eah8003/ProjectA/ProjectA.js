8f73cae2c2bb4c348e6c2f0656c3fd52b7c42333
Evan Allan <evanallan1026@gmail.com>
2021-10-18 14:39:30 -0500

    So close but I gotta go

--- PA/ProjectA_eah8003/ProjectA/ProjectA.js
+++ PA/ProjectA_eah8003/ProjectA/ProjectA.js
@@ -36,7 +36,7 @@ class Vertex {
   static primsPerPos = 4;
   static primsPerColor = 3;
   static primSize;
-  static primType = gl.FLOAT;
+  static primType;
   static get stride() {
     return Vertex.primSize * Vertex.primsPerVertex;
   }
@@ -52,33 +52,33 @@ class Mesh {
 }
 
 class SceneGraphNode {
-  position;
-  rotation;
+  pos;
+  rot;
   scale;
 
   children = [];
   mesh;
 
   constructor(pos, rot, scale, mesh) {
-    if (pos === undefined) {
-      this.position = new Vec3(0, 0, 0);
+    if (pos === undefined || pos === null) {
+      this.pos = new Vec3(0, 0, 0);
     } else {
-      this.position = pos;
+      this.pos = pos;
     }
 
-    if (rot === undefined) {
-      this.rotation = new Vec3(0, 0, 0);
+    if (rot === undefined || rot === null) {
+      this.rot = new Vec3(0, 0, 0);
     } else {
-      this.rotation = rot;
+      this.rot = rot;
     }
 
-    if (scale === undefined) {
+    if (scale === undefined || scale === null) {
       this.scale = new Vec3(1, 1, 1);
     } else {
       this.scale = scale;
     }
 
-    if (mesh !== undefined) {
+    if (mesh !== undefined && mesh !== null) {
       this.mesh = mesh;
     }
   }
@@ -109,22 +109,25 @@ class RenderProgram {
 }
 
 class Context {
-  static gl;
   static canvas;
   static sceneGraph;
   static vboId;
   static renderProgram = new RenderProgram();
+  static fps = 30;
 }
+
+var gl;
 
 function main() {
   Context.canvas = document.getElementById('webgl');
-  Context.gl = getWebGLContext(canvas);
-  if (!Context.gl) {
+  gl = getWebGLContext(Context.canvas);
+  Vertex.primType = gl.FLOAT;
+  if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
   }
 
-  if (!initShaders(Context.gl, Context.vertShader, Context.fragShader)) {
+  if (!initShaders(gl, Context.renderProgram.vertShader, Context.renderProgram.fragShader)) {
     console.log('Failed to intialize shaders.');
     return;
   }
@@ -137,104 +140,113 @@ function main() {
     return;
   }
 
-  Context.gl.clearColor(0.3, 0.3, 0.3, 1.0);
+  gl.clearColor(0.3, 0.3, 0.3, 1.0);
 
-  Context.gl.depthFunc(gl.GREATER);
-  Context.gl.clearDepth(0.0);
-  Context.gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.GREATER);
+  gl.clearDepth(0.0);
+  gl.enable(gl.DEPTH_TEST);
 
-  modelMatrixId = Context.gl.getUniformLocation(Context.gl.program, 'u_ModelMatrix');
+  modelMatrixId = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   Context.renderProgram.attribIds['u_ModelMatrix'] = modelMatrixId;
-  if (!initUniformPointer('u_ModelMatrix') {
+  if (!modelMatrixId) {
     console.log('Failed to get the storage location of u_ModelMatrix');
     return;
   }
+
+  tick();
+}
+
+function animate() {
+
+}
+
+function tick() {
+  animate();
+  drawAll();
+  requestAnimationFrame(tick, Context.canvas);
 }
 
 function initSceneGraph() {
+  var c30 = Math.sqrt(0.75); // cos(30deg) == sqrt(3) / 2 == sqrt(3/4)
+  var sq2 = Math.sqrt(2.0);
+
   tetraMesh = new Mesh();
   tetraMesh.renderType = gl.TRIANGLES;
   tetraMesh.verts = [
-    new Vertex({.pos = new Vec3( 0.0,  0.0, 0.0), .color = new Vec3(1.0, 1.0, 1.0)}),
-    new Vertex({.pos = new Vec3( c30, -0.5, 0.0), .color = new Vec3(0.0, 0.0, 1.0)}),
-    new Vertex({.pos = new Vec3( 0.0,  1.0, 0.0), .color = new Vec3(1.0, 0.0, 0.0)}),
-    new Vertex({.pos = new Vec3( 0.0,  1.0, sq2), .color = new Vec3(1.0, 1.0, 1.0)}),
-    new Vertex({.pos = new Vec3( 0.0,  1.0, 0.0), .color = new Vec3(1.0, 0.0, 0.0)}),
-    new Vertex({.pos = new Vec3(-c30, -0.5, 0.0), .color = new Vec3(1.0, 1.0, 0.0)}),
-    new Vertex({.pos = new Vec3( 0.0,  0.0, sq2), .color = new Vec3(1.0, 1.0, 1.0)}),
-    new Vertex({.pos = new Vec3(-c30, -0.5, 0.0), .color = new Vec3(0.0, 1.0, 0.0)}),
-    new Vertex({.pos = new Vec3( c30, -0.5, 0.0), .color = new Vec3(0.0, 0.0, 1.0)}),
-    new Vertex({.pos = new Vec3(-c30, -0.5, 0.0), .color = new Vec3(0.0, 1.0, 0.0)}),
-    new Vertex({.pos = new Vec3( 0.0,  1.0, 0.0), .color = new Vec3(1.0, 0.0, 0.0)}),
-    new Vertex({.pos = new Vec3( c30, -0.5, 0.0), .color = new Vec3(0.0, 0.0, 1.0)})
+    new Vertex(new Vec3( 0.0,  0.0, 0.0), new Vec3(1.0, 1.0, 1.0)),
+    new Vertex(new Vec3( c30, -0.5, 0.0), new Vec3(0.0, 0.0, 1.0)),
+    new Vertex(new Vec3( 0.0,  1.0, 0.0), new Vec3(1.0, 0.0, 0.0)),
+    new Vertex(new Vec3( 0.0,  1.0, sq2), new Vec3(1.0, 1.0, 1.0)),
+    new Vertex(new Vec3( 0.0,  1.0, 0.0), new Vec3(1.0, 0.0, 0.0)),
+    new Vertex(new Vec3(-c30, -0.5, 0.0), new Vec3(1.0, 1.0, 0.0)),
+    new Vertex(new Vec3( 0.0,  0.0, sq2), new Vec3(1.0, 1.0, 1.0)),
+    new Vertex(new Vec3(-c30, -0.5, 0.0), new Vec3(0.0, 1.0, 0.0)),
+    new Vertex(new Vec3( c30, -0.5, 0.0), new Vec3(0.0, 0.0, 1.0)),
+    new Vertex(new Vec3(-c30, -0.5, 0.0), new Vec3(0.0, 1.0, 0.0)),
+    new Vertex(new Vec3( 0.0,  1.0, 0.0), new Vec3(1.0, 0.0, 0.0)),
+    new Vertex(new Vec3( c30, -0.5, 0.0), new Vec3(0.0, 0.0, 1.0))
   ];
 
   wedgeMesh = new Mesh();
-  wegdeMesh.renderType = gl.TRIANGLES;
+  wedgeMesh.renderType = gl.TRIANGLES;
   wedgeMesh.verts = [
-    new Vertex({.pos = new Vec3( 0.0,  0.0, sq2), .color = new Vec3(1.0, 1.0, 1.0)}),
-    new Vertex({.pos = new Vec3(-c30, -0.5, 0.0), .color = new Vec3(0.0, 1.0, 0.0)}),
-    new Vertex({.pos = new Vec3( c30, -0.5, 0.0), .color = new Vec3(0.0, 0.0, 1.0)}),
-    new Vertex({.pos = new Vec3(-c30, -0.5, 0.0), .color = new Vec3(0.0, 1.0, 0.0)}),
-    new Vertex({.pos = new Vec3( 0.0,  1.0, 0.0), .color = new Vec3(1.0, 0.0, 0.0)}),
-    new Vertex({.pos = new Vec3( c30, -0.5, 0.0), .color = new Vec3(0.0, 0.0, 1.0)})
+    new Vertex(new Vec3( 0.0,  0.0, sq2), new Vec3(1.0, 1.0, 1.0)),
+    new Vertex(new Vec3(-c30, -0.5, 0.0), new Vec3(0.0, 1.0, 0.0)),
+    new Vertex(new Vec3( c30, -0.5, 0.0), new Vec3(0.0, 0.0, 1.0)),
+    new Vertex(new Vec3(-c30, -0.5, 0.0), new Vec3(0.0, 1.0, 0.0)),
+    new Vertex(new Vec3( 0.0,  1.0, 0.0), new Vec3(1.0, 0.0, 0.0)),
+    new Vertex(new Vec3( c30, -0.5, 0.0), new Vec3(0.0, 0.0, 1.0))
   ];
 
   squareMesh = new Mesh();
   squareMesh.renderType = gl.TRIANGLE_FAN
   squareMesh.verts = [
-    new Vertex({.pos = new Vec3(-0.5, -0.5, 0.0), .color = new Vec3(1.0, 0.0, 0.0)}),
-    new Vertex({.pos = new Vec3(-0.5,  0.5, 0.0), .color = new Vec3(0.0, 1.0, 0.0)}),
-    new Vertex({.pos = new Vec3( 0.5,  0.5, 0.0), .color = new Vec3(0.0, 0.0, 1.0)}),
-    new Vertex({.pos = new Vec3( 0.5, -0.5, 0.0), .color = new Vec3(1.0, 1.0, 1.0)})
+    new Vertex(new Vec3(-0.5, -0.5, 0.0), new Vec3(1.0, 0.0, 0.0)),
+    new Vertex(new Vec3(-0.5,  0.5, 0.0), new Vec3(0.0, 1.0, 0.0)),
+    new Vertex(new Vec3( 0.5,  0.5, 0.0), new Vec3(0.0, 0.0, 1.0)),
+    new Vertex(new Vec3( 0.5, -0.5, 0.0), new Vec3(1.0, 1.0, 1.0))
   ];
 
   triangleMesh = new Mesh();
   triangleMesh.renderType = gl.TRIANGLES;
   triangleMesh.verts = [
-    new Vertex({.pos = new Vec3(-0.5, -0.5, 0.0), .color = new Vec3(0.0, 1.0, 1.0)}),
-    new Vertex({.pos = new Vec3(-0.5,  0.5, 0.0), .color = new Vec3(1.0, 0.0, 1.0)}),
-    new Vertex({.pos = new Vec3( 0.5,  0.5, 0.0), .color = new Vec3(1.0, 1.0, 0.0)}),
+    new Vertex(new Vec3(-0.5, -0.5, 0.0), new Vec3(0.0, 1.0, 1.0)),
+    new Vertex(new Vec3(-0.5,  0.5, 0.0), new Vec3(1.0, 0.0, 1.0)),
+    new Vertex(new Vec3( 0.5,  0.5, 0.0), new Vec3(1.0, 1.0, 0.0))
   ];
 
   topNode = new SceneGraph();
 
-  tetraParentNode = new SceneGraphNode();
-  tetraNode = new SceneGraphNode();
-  squareNode = new SceneGraphNode();
+  tetraParentNode = new SceneGraphNode(new Vec3(-0.4, -0.4, 0.0),   new Vec3(0, 0, 0),        new Vec3(0.5, 0.5, 0.5),  null);
+  tetraNode       = new SceneGraphNode(null,                        null,                     null,                     tetraMesh);
+  square1Node     = new SceneGraphNode(new Vec3(-1.1, -0.75, 0.0),  new Vec3(-50, 50, -100),  new Vec3(0.5, 0.5, 0.5),  squareMesh);
+  square2Node     = new SceneGraphNode(new Vec3(0.0, 0.0, 1.75),    new Vec3(90, 90, 0),      new Vec3(0.5, 0.5, 0.5),  squareMesh);
+  square3Node     = new SceneGraphNode(new Vec3(-1.1, -0.75, 0.0),  new Vec3(50, 50, -100),   new Vec3(0.5, 0.5, 0.5),  squareMesh);
 
-  dragObjNode = new SceneGraphNode();
-  dragTetraNode = new SceneGraphNode();
-  triangleNode = new SceneGraphNode();
+  dragObjNode   = new SceneGraphNode(new Vec3(0, 0.5, 0.5),  null,                     null,  null);
+  dragTetraNode = new SceneGraphNode(null,                   null,                     null,  wedgeMesh);
+  triangleNode  = new SceneGraphNode(null,                   new Vec3(-25, -25, 135),  null,  triangleMesh});
 
   topNode.children = [tetraParentNode, dragObjNode];
-  tetraParentNode.children = [tetraNode, squareNode];
+  tetraParentNode.children = [tetraNode, square1Node, square2Node, square3Node];
   dragObjNode.children = [dragTetraNode, triangleNode];
-
-  tetraNode.mesh = tetraMesh;
-  dragTetraNode.mesh = wedgeMesh;
-  squareNode.mesh = squareMesh;
-  triangleNode.mesh = triangleMesh;
 
   Context.sceneGraph = topNode;
 }
 
 function buildBuffer(graphNode, currBuffer) {
   if (graphNode.mesh) {
-    mesh.vboStart = currBuffer.length;
-    for (vertex in graphNode.mesh.verts) {
-      currBuffer.append(vertex.pos.x);
-      currBuffer.append(vertex.pos.y);
-      currBuffer.append(vertex.pos.z);
-      currBuffer.append(1.0);
-      currBuffer.append(vertex.color.r);
-      currBuffer.append(vertex.color.g);
-      currBuffer.append(vertex.color.b);
+    graphNode.mesh.vboStart = currBuffer.length;
+    for (vertex of graphNode.mesh.verts) {
+      if (vertex.color === undefined) {
+        console.log(vertex);
+      }
+      currBuffer.push(vertex.pos.x, vertex.pos.y, vertex.pos.z, 1.0, vertex.color.r, vertex.color.g, vertex.color.b);
     }
     mesh.vboCount = (currBuffer.length - mesh.vboStart) / Vertex.primsPerVertex;
   }
 
-  for (child in graphNode.children) {
+  for (child of graphNode.children) {
     buildBuffer(child, currBuffer);
   }
 
@@ -246,7 +258,7 @@ function initVertexBuffer() {
   var buffer = new Float32Array(bufferValues);
 
   // Create a buffer object
-  var Context.vboId = gl.createBuffer();
+  Context.vboId = gl.createBuffer();
   if (!Context.vboId) {
     console.log('Failed to create the shape buffer object');
     return false;
@@ -263,7 +275,7 @@ function initVertexBuffer() {
     console.log('Failed to get the storage location of a_Position');
     return -1;
   }
-  gl.vertexAttribPointer(a_Position, Vertex,primsPerPos, Vertex.primType, false /* Normalize */, Vertex.stride, 0);
+  gl.vertexAttribPointer(a_Position, Vertex.primsPerPos, Vertex.primType, false /* Normalize */, Vertex.stride, 0);
   gl.enableVertexAttribArray(a_Position);
 
   var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
@@ -289,12 +301,12 @@ function drawNode(modelMatrix, node) {
   modelMatrix.rotate(node.rot.z, 1, 0, 0);
 
   if (node.mesh) {
-    Context.gl.uniformMatrix4fv(Context.renderProgram.attribIds['u_ModelMatrix'], false, modelMatrix.elements);
-    Context.gl.drawArrays(node.mesh.renderType, node.mesh.vboStart, node.mesh.vboCount);
+    gl.uniformMatrix4fv(Context.renderProgram.attribIds['u_ModelMatrix'], false, modelMatrix.elements);
+    gl.drawArrays(node.mesh.renderType, node.mesh.vboStart, node.mesh.vboCount);
   }
 
-  for (child in node.children) {
-    modelMatrix = drawNode(modelMatrix, node);
+  for (child of node.children) {
+    modelMatrix = drawNode(modelMatrix, child);
   }
   return popMatrix();
 }
@@ -304,48 +316,8 @@ function drawAll() {
   modelMatrix = new Matrix4();
   drawNode(modelMatrix, Context.sceneGraph);
 
-  g_modelMatrix.setTranslate(-0.4,-0.4, 0.0);
-  g_modelMatrix.scale(1,1,-1);
-  g_modelMatrix.scale(0.5, 0.5, 0.5);
-  g_modelMatrix.rotate(g_angle01, 0, 1, 0);
-  g_modelMatrix.rotate(g_angle02, 1, 0, 0);
-
-  gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
-  drawTetra();
-
-  pushMatrix(g_modelMatrix);
-  g_modelMatrix.scale(0.5, 0.5, 0.5);
-  g_modelMatrix.translate(2.2, -1.3, 0.0);
-  g_modelMatrix.rotate(100, 0, 0, 1);
-  g_modelMatrix.rotate(50, 1, 1, 0);
-  gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
-  drawSquare();
-  g_modelMatrix = popMatrix();
-
-  pushMatrix(g_modelMatrix);
-  g_modelMatrix.scale(0.5, 0.5, 0.5);
-  g_modelMatrix.translate(-2.2, -1.3, 0.0);
-  g_modelMatrix.rotate(-100, 0, 0, 1);
-  g_modelMatrix.rotate(50, -1, 1, 0);
-  gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
-  drawSquare();
-  g_modelMatrix = popMatrix();
-
-  pushMatrix(g_modelMatrix);
-  g_modelMatrix.scale(0.5, 0.5, 0.5);
-  g_modelMatrix.translate(0, 0, 3.5);
-  //g_modelMatrix.rotate(-100, 0, 0, 1);
-  g_modelMatrix.rotate(90, 1, 1, 0);
-  gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
-  drawSquare();
-  g_modelMatrix = popMatrix();
-
-  g_modelMatrix.setTranslate(0.4, 0.4, 0.0);
-  g_modelMatrix.scale(1,1,-1);
-  g_modelMatrix.scale(0.3, 0.3, 0.3);
-
-  var dist = Math.sqrt(g_xMdragTot*g_xMdragTot + g_yMdragTot*g_yMdragTot);
-  g_modelMatrix.rotate(dist*120.0, -g_yMdragTot+0.0001, g_xMdragTot+0.0001, 0.0);
+  //var dist = Math.sqrt(g_xMdragTot*g_xMdragTot + g_yMdragTot*g_yMdragTot);
+  //g_modelMatrix.rotate(dist*120.0, -g_yMdragTot+0.0001, g_xMdragTot+0.0001, 0.0);
 
   pushMatrix(g_modelMatrix);
   g_modelMatrix.translate(0, 0.5, 0.5);
@@ -358,23 +330,3 @@ function drawAll() {
   gl.uniformMatrix4fv(g_modelMatLoc, false, g_modelMatrix.elements);
   drawWedge();
 }
-
-var g_last = Date.now();
-
-function animate() {
-  var now = Date.now();
-  var elapsed = now - g_last;
-  g_last = now;
-
-  g_angle01 = g_angle01 + (g_angle01Rate * elapsed) / 1000.0;
-  if(g_angle01 > 180.0) g_angle01 = g_angle01 - 360.0;
-  if(g_angle01 <-180.0) g_angle01 = g_angle01 + 360.0;
-
-  g_angle02 = g_angle02 + (g_angle02Rate * elapsed) / 1000.0;
-  if(g_angle02 > 180.0) g_angle02 = g_angle02 - 360.0;
-  if(g_angle02 <-180.0) g_angle02 = g_angle02 + 360.0;
-  
-  if(g_angle02 > 45.0 && g_angle02Rate > 0) g_angle02Rate *= -1.0;
-  if(g_angle02 < 0.0  && g_angle02Rate < 0) g_angle02Rate *= -1.0;
-}
-
